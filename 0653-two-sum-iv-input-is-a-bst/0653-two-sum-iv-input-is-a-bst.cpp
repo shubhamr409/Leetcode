51d99e975e161a2cd5015ea96d0b12fd2719092a
Shubham Rajpurohit <120755054+shubhamr409@users.noreply.github.com>
2026-07-06 01:04:55 +0530

    Time: 0 ms (100%), Space: 37.3 MB (64.21%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -11,23 +11,54 @@
  */
 class Solution {
 public:
-vector<int> temp;
-    void inOrder(TreeNode* root){
-        if(root == NULL) return;
-        inOrder(root->left);
-        temp.push_back(root->val);
-        inOrder(root->right);
-        return;
+    stack<TreeNode*> asc;
+    stack<TreeNode*> desc;
+    TreeNode* getSmall(){
+        if (asc.empty()) return NULL;
+        TreeNode* small = asc.top();
+        asc.pop();
+        TreeNode* rightChild = small->right;
+        while (rightChild){
+            asc.push(rightChild);
+            rightChild = rightChild->left;
+        }
+        return small;
     }
+
+    TreeNode* getBig(){
+        if (desc.empty()) return NULL;
+        TreeNode* big = desc.top();
+        desc.pop();
+        TreeNode* leftChild = big->left;
+        while (leftChild){
+            desc.push(leftChild);
+            leftChild = leftChild->right;
+        }
+        return big;
+    }
+
     bool findTarget(TreeNode* root, int k) {
-        inOrder(root);
-        int n = temp.size();
-        int i = 0, j = n-1;
-        while(i < j){
-            int sum = temp[i] + temp[j];
+        if (root == NULL) return false;
+        TreeNode* t = root;
+        while (t){
+            asc.push(t);
+            t = t->left;
+        }
+        t = root;
+        while (t) {
+            desc.push(t);
+            t = t->right;
+        }
+
+        TreeNode* i = getSmall();
+        TreeNode* j = getBig();
+        while (i && j && i != j && i->val < j->val) {
+            int sum = i->val + j->val;
             if (sum == k) return true;
-            if (sum < k) i++;
-            else if(sum > k) j--;
+            if (sum > k)
+                j = getBig();
+            else
+                i = getSmall();
         }
         return false;
     }
