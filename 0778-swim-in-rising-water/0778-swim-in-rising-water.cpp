1ba27b7af34c47349e4a11d49bf5922557a76df6
Shubham Rajpurohit <120755054+shubhamr409@users.noreply.github.com>
2026-07-21 23:47:42 +0530

    Time: 12 ms (33.55%), Space: 17.5 MB (8.42%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,45 +1,56 @@
 class Solution {
 public:
-    bool valid(int i, int j, int n){
-        if(i < 0 || i >= n || j < 0 || j >= n) return false;
+    bool valid(int i, int j, int n, int m){
+        if(i < 0 || i >= n || j < 0 || j >= m) return false;
         return true;
     }
-    int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size();
-        vector<vector<int>> res(n);
-        for(int i = 0; i < n; i++){
-            vector<int> t(n, INT_MAX);
-            res[i] = t;
-        }
-
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+    bool possibleToReach(vector<vector<int>>& grid, int n, int m, int elevation){
         int x[4] = {1, -1, 0, 0};
         int y[4] = {0, 0, 1, -1};
-        res[0][0] = grid[0][0];
-        pq.push({grid[0][0], {0, 0}});
-
-        while(!pq.empty()){
-            pair<int, pair<int, int>> p = pq.top();
-            pq.pop();
-            int currTime = p.first;
-            int row = p.second.first;
-            int col = p.second.second;
-
-            if (row == n - 1 && col == n - 1)
-                return currTime;
-            
-            if(currTime > res[row][col]) continue;
+        queue<pair<int, int>> q;
+        vector<vector<int>> vis(n);
+        for(int i = 0; i < n; i++){
+            vector<int> t(m, 0);
+            vis[i] = t;
+        }
+        q.push({0, 0});
+        vis[0][0] = 1;
+        while(!q.empty()){
+            pair<int, int> p = q.front();
+            q.pop();
+            int row = p.first;
+            int col = p.second;
+            if(row == n-1 && col == m-1) return true;
             for(int k = 0; k < 4; k++){
                 int r = row + x[k];
                 int c = col + y[k];
-                if(!valid(r, c, n)) continue;
-                int nextTime = max(currTime, grid[r][c]);
-                if(nextTime < res[r][c]){
-                    res[r][c] = nextTime;
-                    pq.push({nextTime, {r, c}});
+                if(valid(r, c, n, m) && vis[r][c] == 0 && elevation >= grid[r][c]){
+                    q.push({r, c});
+                    vis[r][c] = 1;
                 }
             }
         }
-        return -1;
+        return false;
+    }
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int low = grid[0][0];
+        int high = grid[0][0];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                high = max(high, grid[i][j]);
+            }
+        }
+        int res = 0;
+        while(low <= high){
+            int mid = low + (high-low) / 2;
+            if(possibleToReach(grid, n, m, mid)){
+                res = mid;
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return res;
     }
 };
