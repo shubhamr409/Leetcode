1653a63ded1224afd7bf97488c123837dc241217
Shubham Rajpurohit <120755054+shubhamr409@users.noreply.github.com>
2026-08-18 11:01:14 +0530

    Time: 0 ms (100%), Space: 28.9 MB (84.21%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -2,36 +2,33 @@ class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
         int n = nums.size();
-        map<int, int> mpp;
-        int max_ele = INT_MIN;
-        for(int num : nums){
-            mpp[num]++;
-            max_ele = max(max_ele, num);
-        } 
-        if(k == n){
-            return max_ele;
+        vector<int> freq(51, 0);
+        for (int x : nums) {
+            freq[x]++;
         }
-        else if(k == 1){
-            for (auto it = mpp.rbegin(); it != mpp.rend(); ++it){
-                if (it->second == 1){
-                    return it->first;
+
+        if (k == n) {
+            return *max_element(nums.begin(), nums.end());
+        }
+
+        if (k == 1) {
+            int ans = -1;
+            for (int x : nums) {
+                if (freq[x] == 1) {
+                    ans = max(ans, x);
                 }
             }
+            return ans;
         }
-        else if(k > 1 && k < n){
-            int ele1 = nums[0];
-            int ele2 = nums[n-1];
-            if(ele1 > ele2){
-                if(mpp[ele1] == 1) return ele1;
-                else if(mpp[ele2] == 1) return ele2;
-                else return -1;
-            }
-            else{
-                if(mpp[ele2] == 1) return ele2;
-                else if(mpp[ele1] == 1) return ele1;
-                else return -1;
-            }
+
+        int ans = -1;
+        if (freq[nums[0]] == 1) {
+            ans = max(ans, nums[0]);
         }
-        return -1;
+        if (freq[nums[n - 1]] == 1) {
+            ans = max(ans, nums[n - 1]);
+        }
+
+        return ans;
     }
 };
