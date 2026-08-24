5bedd9aa3fcd671138ca1e886e93ee0dbc240e8d
Shubham Rajpurohit <120755054+shubhamr409@users.noreply.github.com>
2026-08-24 16:20:55 +0530

    Time: 104 ms (66.91%), Space: 95.4 MB (43.78%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+int n;
+
+    int stoneGameVIII(vector<int>& stones) {
+        n = stones.size();
+        vector<int> prefixSum(n, 0);
+        prefixSum[0] = stones[0];
+        for(int i = 1; i < n; i++){
+            prefixSum[i] = prefixSum[i-1] + stones[i];
+        }
+        
+        vector<int> t(n);
+        t[n - 1] = prefixSum[n - 1];
+        for (int i = n - 2; i >= 1; i--) {
+            int take = prefixSum[i] - t[i + 1];
+            int skip = t[i+1];
+
+            t[i] = max(take, skip);
+        }
+
+        return t[1]; 
+    }
+};
