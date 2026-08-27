0941ee4bd3324cada12c6c1db8d994b0ecc03ac1
Shubham Rajpurohit <120755054+shubhamr409@users.noreply.github.com>
2026-08-28 01:48:40 +0530

    Time: 3 ms (70.24%), Space: 9.7 MB (94.05%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-string result = "";
-
-    bool solve(string& curr, vector<int>& count, string& target, int i, bool greater) {
-        if(i == target.length()) {
-            if(greater) {
-                result = curr;
-                return true;
-            }
-            return false;
-        }
-        for(char ch = 'a'; ch <= 'z'; ch++) {
-            if(count[ch-'a'] == 0)
-                continue;
-            if(greater == false && ch < target[i])
-                continue;
-            curr.push_back(ch);
-            count[ch-'a']--;
-            bool isGreater = greater || ch > target[i];
-            if(solve(curr, count, target, i+1, isGreater)) {
-                return true;
-            }
-            curr.pop_back();
-            count[ch-'a']++;
-        }
-        return false;
-    }
-
     string lexGreaterPermutation(string s, string target) {
+        int n = s.length();
         vector<int> count(26, 0);
-        for(char &ch : s)
-            count[ch-'a']++;
-        string curr;
-        solve(curr, count, target, 0, false);
-        return result;
+        for (char c : s) {
+            count[c - 'a']++;
+        }
+
+        int matched_len = 0;
+        while (matched_len < n && count[target[matched_len] - 'a'] > 0) {
+            count[target[matched_len] - 'a']--;
+            matched_len++;
+        }
+
+        for (int i = min(matched_len, n-1); i >= 0; i--) {
+            if(i < matched_len) count[target[i] - 'a']++;
+
+            for (char ch = target[i] + 1; ch <= 'z'; ch++) {
+                if (count[ch - 'a'] > 0) {
+                    count[ch - 'a']--;
+
+                    string result = target.substr(0, i);
+                    result.push_back(ch);
+
+                    for (char rem = 'a'; rem <= 'z'; rem++) {
+                        while (count[rem - 'a'] > 0) {
+                            result.push_back(rem);
+                            count[rem - 'a']--;
+                        }
+                    }
+                    return result;
+                }
+            }
+        }
+
+        return "";
     }
 };
