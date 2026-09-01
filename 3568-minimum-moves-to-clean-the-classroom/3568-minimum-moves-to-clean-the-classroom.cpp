3f3e7c063d0493c49842ae40589bd80996f2e972
Shubham Rajpurohit <120755054+shubhamr409@users.noreply.github.com>
2026-09-02 01:05:36 +0530

    Time: 1151 ms (48%), Space: 390.3 MB (38%) - LeetHub

--- 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.cpp
+++ 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.cpp
@@ -1,64 +1,72 @@
 class Solution {
 public:
     int minMoves(vector<string>& classroom, int energy) {
-        int m = classroom.size(), n = classroom[0].size();
-        int litter_id[20][20];
-        memset(litter_id, -1, sizeof(litter_id));
-        
-        int sr = 0, sc = 0, k = 0;
+        int m = classroom.size();
+        int n = classroom[0].size();
+        int litterBit[20][20];
+        int max_energy = energy;
+        int litterCount = 0;
+        int startRow = 0;
+        int startCol = 0;
         for (int r = 0; r < m; ++r) {
             for (int c = 0; c < n; ++c) {
+                litterBit[r][c] = -1;
                 if (classroom[r][c] == 'S') {
-                    sr = r; sc = c;
+                    startRow = r;
+                    startCol = c;
                 } else if (classroom[r][c] == 'L') {
-                    litter_id[r][c] = k++;
+                    litterBit[r][c] = litterCount;
+                    litterCount++;
                 }
             }
         }
+
+        int allCollected = (1 << litterCount) - 1;
+        if (litterCount == 0) 
+            return 0;
         
-        if (k == 0) return 0;
-        int total_mask = (1 << k) - 1;
+        vector<vector<vector<vector<bool>>>> seen(m, vector<vector<vector<bool>>>(n, vector<vector<bool>>(max_energy+1, vector<bool>(1 << litterCount, false))));
         
-        // best_energy[r][c][mask] stores the max energy seen for this state
-        vector<vector<vector<int>>> best_energy(m, vector<vector<int>>(n, vector<int>(1 << k, -1)));
-        
-        // Queue elements: {row, col, mask, energy, moves}
-        queue<tuple<int, int, int, int, int>> q;
-        q.push({sr, sc, 0, energy, 0});
-        best_energy[sr][sc][0] = energy;
+        queue<tuple<int, int, int, int>> q;
+        q.push({startRow, startCol, max_energy, 0});
+        seen[startRow][startCol][max_energy][0] = true;
+
+        int moves = 0;
         
         const int dr[] = {-1, 1, 0, 0};
         const int dc[] = {0, 0, -1, 1};
         
         while (!q.empty()) {
-            auto [r, c, mask, e, moves] = q.front();
-            q.pop();
-            
-            for (int d = 0; d < 4; ++d) {
-                int nr = r + dr[d], nc = c + dc[d];
+            int currSize = q.size();
+            while(currSize--){
+                auto [r, c, e, mask] = q.front();
+                q.pop();
                 
-                if (nr < 0 || nr >= m || nc < 0 || nc >= n) continue;
-                if (classroom[nr][nc] == 'X') continue;
-                
-                int next_e = e - 1;
-                if (next_e < 0) continue;
-                
-                int next_mask = mask;
-                if (classroom[nr][nc] == 'R') {
-                    next_e = energy;
-                } else if (classroom[nr][nc] == 'L') {
-                    next_mask |= (1 << litter_id[nr][nc]);
+                if(mask == allCollected) return moves;
+                if(e == 0) continue;
+                for (int d = 0; d < 4; ++d) {
+                    int nr = r + dr[d], nc = c + dc[d];
+                    
+                    if (nr < 0 || nr >= m || nc < 0 || nc >= n) continue;
+                    if (classroom[nr][nc] == 'X') continue;
+                    
+                    int next_e = e - 1;     
+                    int next_mask = mask;
+
+                    if (classroom[nr][nc] == 'R') {
+                        next_e = energy;
+                    } else if (classroom[nr][nc] == 'L') {
+                        next_mask |= (1 << litterBit[nr][nc]);
+                    }
+
+                    if(!seen[nr][nc][next_e][next_mask]){
+                            seen[nr][nc][next_e][next_mask] = true;
+                            q.push({nr, nc, next_e, next_mask});
+                    }
                 }
-                
-                if (next_mask == total_mask) return moves + 1;
-                
-                if (next_e <= best_energy[nr][nc][next_mask]) continue;
-                best_energy[nr][nc][next_mask] = next_e;
-                
-                q.push({nr, nc, next_mask, next_e, moves + 1});
             }
+            moves++;
         }
-        
         return -1;
     }
 };
