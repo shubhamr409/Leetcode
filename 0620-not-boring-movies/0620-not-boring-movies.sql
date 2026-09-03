bced65664cf6779cd4b7bb60a96d01f187e0e662
Shubham Rajpurohit <120755054+shubhamr409@users.noreply.github.com>
2026-09-03 19:21:11 +0530

    Time: 302 ms (37.95%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-SELECT id, movie, description, rating 
-FROM Cinema
+SELECT * FROM Cinema
 WHERE (id % 2 <> 0 AND description <> "boring")
 ORDER BY rating DESC
