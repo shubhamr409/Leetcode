638888ff56111f4265634366d2cfc17f2aab6429
Shubham Rajpurohit <120755054+shubhamr409@users.noreply.github.com>
2026-09-04 16:21:32 +0530

    Time: 1277 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,9 +1,11 @@
-# Write your MySQL query statement below
-SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = "Y" OR employee_id IN(
-    SELECT employee_id
+WITH RankedEmployees AS (
+    SELECT 
+        employee_id, 
+        department_id, 
+        primary_flag,
+        COUNT(department_id) OVER(PARTITION BY employee_id) AS dept_count
     FROM Employee
-    GROUP BY employee_id
-    HAVING COUNT(department_id) = 1
-    )
+)
+SELECT employee_id, department_id
+FROM RankedEmployees
+WHERE primary_flag = 'Y' OR dept_count = 1;
