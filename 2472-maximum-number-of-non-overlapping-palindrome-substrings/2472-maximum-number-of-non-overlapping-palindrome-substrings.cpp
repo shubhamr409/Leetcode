96f013b20982c3d73ef76ba9162991a0d09d57da
Shubham Rajpurohit <120755054+shubhamr409@users.noreply.github.com>
2026-09-15 14:39:24 +0530

    Time: 1893 ms (5.49%), Space: 208.1 MB (18.06%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -10,23 +10,24 @@ int n;
         }
         return true;
     }
-    int solve(string& s, int k, int i, int j, vector<vector<int>>& t){
-        if(i >= n || j >= n) return 0;
-        if(t[i][j] != -1) return t[i][j];
-        if(isPalindrome(s, i, j)){
-            int take = 1 + solve(s, k, j+1, j+k, t);
-            int grow = solve(s, k, i, j+1, t);
-            int slide = solve(s, k, i+1, j+1, t);
-            return t[i][j] = max({take, grow, slide});
-        }
-        int grow = solve(s, k, i, j+1, t);
-        int slide = solve(s, k, i+1, j+1, t);
-        return t[i][j] = max(grow, slide);
-    }
     int maxPalindromes(string s, int k) {
         n = s.size();
         if(k == 1) return n;
-        vector<vector<int>> t(n+1, vector<int>(n+1, -1));
-        return solve(s, k, 0, k-1, t);
+        vector<vector<int>> t(n+1, vector<int>(n+1));
+
+        for(int i = n-1; i >= 0; i--){
+            for(int j = n-1; j >= 0; j--){
+                if(isPalindrome(s, i, j)){
+                    int take = 1 + (j+k <=n ? t[j+1][j+k] : 0);
+                    int grow = t[i][j+1];
+                    int slide = t[i+1][j+1];
+                    t[i][j] = max({take, grow, slide});
+                }
+                int grow = t[i][j+1];
+                int slide = t[i+1][j+1];
+                t[i][j] = max({t[i][j], grow, slide});
+            }
+        }
+        return t[0][k-1];
     }
 };
