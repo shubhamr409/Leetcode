fde563def7d7f05cf635b6fe0fa0d379fabb8ade
Shubham Rajpurohit <120755054+shubhamr409@users.noreply.github.com>
2026-09-17 12:01:08 +0530

    Time: 23 ms (47.94%), Space: 92.2 MB (54.9%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -2,29 +2,43 @@ class Solution {
 public:
     int minSumOfLengths(vector<int>& arr, int target) {
         int n = arr.size();
-        int i = 0, j = 0;
-        int currSum = 0;
-        vector<int> minBestLenTillIdx(n, INT_MAX);
+        const int INF = 1e9;
+        
+        vector<int> prefix(n, INF);
+        vector<int> suffix(n, INF);
 
-        int bestMinLen = INT_MAX;
-        int result = INT_MAX;
-        while(j < n){
-            currSum += arr[j];
-            while(i < j && currSum > target){
-                currSum -= arr[i];
-                i++;
+        int sum = 0, left = 0;
+        for (int right = 0; right < n; ++right) {
+            sum += arr[right];
+            while (sum > target && left <= right) {
+                sum -= arr[left++];
             }
-
-            if(currSum == target){
-                int len = j-i+1;
-                if(i > 0 && minBestLenTillIdx[i-1] != INT_MAX){
-                    result = min(result, len + minBestLenTillIdx[i-1]);
-                }
-                bestMinLen = min(bestMinLen, len);
+            if (right > 0) prefix[right] = prefix[right - 1];
+            if (sum == target) {
+                prefix[right] = min(prefix[right], right - left + 1);
             }
-            minBestLenTillIdx[j] = bestMinLen;
-            j++;
         }
-        return result == INT_MAX ? -1 : result;
+
+        sum = 0;
+        int right = n - 1;
+        for (int i = n - 1; i >= 0; --i) {
+            sum += arr[i];
+            while (sum > target && right >= i) {
+                sum -= arr[right--];
+            }
+            if (i + 1 < n) suffix[i] = suffix[i + 1];
+            if (sum == target) {
+                suffix[i] = min(suffix[i], right - i + 1);
+            }
+        }
+
+        int ans = INF;
+        for (int i = 0; i < n - 1; ++i) {
+            if (prefix[i] != INF && suffix[i + 1] != INF) {
+                ans = min(ans, prefix[i] + suffix[i + 1]);
+            }
+        }
+
+        return ans >= INF ? -1 : ans;
     }
 };
