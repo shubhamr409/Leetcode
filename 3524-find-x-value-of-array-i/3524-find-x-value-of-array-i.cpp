8438c65e98ae9ff4ff6b1c9e021c2f482af7de63
Shubham Rajpurohit <120755054+shubhamr409@users.noreply.github.com>
2026-09-21 16:24:36 +0530

    Time: 519 ms (13.39%), Space: 339 MB (8.93%) - LeetHub

--- 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
-    vector<long long> resultArray(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<long long> result(k, 0);
-        vector<long long> prevCount(k, 0);
+    using ll = long long;
+    int n;
+    ll solve(int i, int prevProd, int req, int k, auto& nums, auto& dp) {
+        if(i >= nums.size()) return 0;
+        if(dp[i][prevProd] != -1) return dp[i][prevProd];
 
-        for(int i = 0; i < n; i++){
-            //index i par end hone waale all subarrays
-            vector<long long> currCount(k, 0);
-            int currElementRemainder = nums[i]%k;
-            currCount[currElementRemainder]++;
+        ll skip = 0, take = 0;
 
-            for(int oldRem = 0; oldRem <= k-1; oldRem++){
-                int newRemain = ((long long)oldRem * nums[i] % k) % k;
-                currCount[newRemain] += prevCount[oldRem];
-            }
-
-            prevCount = move(currCount);
-
-            for(int x = 0; x <= k-1; x++){
-                result[x] += prevCount[x];
-            }
+        // Skip current element (only if no subarray started yet)
+        if(prevProd == k){
+            skip = solve(i + 1, k, req, k, nums, dp);
         }
 
-        return result;
+        // Take current element
+        ll curProd;
+        if(prevProd == k) curProd = nums[i]; // started from cur num
+        else curProd = (prevProd * nums[i]) % k; // extending forward
+
+        take += (curProd == req);
+        take += solve(i + 1, curProd, req, k, nums, dp);
+
+        return dp[i][prevProd] = take + skip;
+    }
+
+    vector<ll> resultArray(vector<int>& nums, int k) {
+        n = nums.size();
+        vector<ll> res(k, 0);
+
+        for(int& num : nums) num %= k;
+
+        for(int r = 0; r < k; r++){
+            vector<vector<ll>> dp(n, vector<ll>(k + 1, -1));
+            res[r] = solve(0, k, r, k, nums, dp);
+        }
+        return res;
     }
 };
