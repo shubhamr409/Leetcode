9e4fb8700b99760e10e0627cc87065be8f4abbfe
Shubham Rajpurohit <120755054+shubhamr409@users.noreply.github.com>
2026-09-24 19:53:11 +0530

    Time: 4 ms (41.79%), Space: 68.3 MB (53.68%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,24 +2,29 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> L2R(n, 1);
-        vector<int> R2L(n, 1);
 
-        for(int i = 1; i < n; i++){
-            if(ratings[i] > ratings[i-1]){
-                L2R[i] = max(L2R[i], L2R[i-1] + 1);
+        int count = n;
+        int i = 1;
+        while(i < n){
+            if(ratings[i] == ratings[i-1]){
+                i++;
+                continue;
             }
-        }
-
-        for(int i = n-2; i >= 0; i--){
-            if(ratings[i] > ratings[i+1]){
-                R2L[i] = max(R2L[i], R2L[i+1] + 1);
+            int peak = 0;
+            while(ratings[i] > ratings[i-1]){
+                peak++;
+                count += peak;
+                i++;
+                if(i == n) return count;
             }
+            int dip = 0;
+            while(i < n && ratings[i] < ratings[i-1]){
+                dip++;
+                count += dip;
+                i++;
+            }
+            count -= min(dip, peak);
         }
-        int ans = 0;
-        for(int i = 0; i < n; i++){
-            ans += max(L2R[i], R2L[i]);
-        }
-        return ans;
+        return count;
     }
 };
