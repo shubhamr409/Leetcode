e604a33321c6403385fa37aca616276b68510a7e
Shubham Rajpurohit <120755054+shubhamr409@users.noreply.github.com>
2026-09-24 22:51:15 +0530

    Time: 67 ms (37%), Space: 43.7 MB (92.84%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,14 +9,14 @@
  */
 class Solution {
 public:
-    TreeNode* ans = NULL;
+TreeNode* ans = NULL;
     int find(TreeNode* node, TreeNode* p, TreeNode* q){
         if(node == NULL) return 0;
         int left = find(node->left, p, q);
         int right = find(node->right, p, q);
         int self = 0;
         if(node == p || node == q) self = 1;
-        int total = left + right + self;
+        int total = self + left + right;
         if(total == 2 && ans == NULL) ans = node;
         return total;
     }
