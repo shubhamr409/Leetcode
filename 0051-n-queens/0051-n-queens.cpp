4ccbe3b44e580a2d27e1c1c6ac2b73e4644bc852
Shubham Rajpurohit <120755054+shubhamr409@users.noreply.github.com>
2026-09-26 20:22:03 +0530

    Time: 11 ms (11.12%), Space: 12.1 MB (16.54%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,21 +1,10 @@
 class Solution {
 public:
 vector<vector<string>> result;
+unordered_set<int> cols;
+unordered_set<int> diags;
+unordered_set<int> antiDiags;
 
-    bool isValid(vector<string>& board, int row, int col){
-        for(int i = row; i >= 0; i--){
-            if(board[i][col] == 'Q') return false;
-        }
-
-        for(int i = row, j = col; i >= 0 && j >= 0; i--, j--){
-            if(board[i][j] == 'Q') return false;
-        }
-
-        for(int i = row, j = col; i>= 0 && j < board.size(); i--, j++){
-            if(board[i][j] == 'Q') return false;
-        }
-        return true;
-    }
 
     void solve(vector<string>& board, int row){
         if(row == board.size()){
@@ -23,12 +12,23 @@ vector<vector<string>> result;
             return;
         }
 
-        for(int i = 0; i < board.size(); i++){
-            if(isValid(board, row, i)){
-                board[row][i] = 'Q';
-                solve(board, row+1);
-                board[row][i] = '.';
-            }
+        for(int col = 0; col < board.size(); col++){
+            int diag_const = row + col;
+            int antidiag_const = row - col;
+
+            if(cols.find(col) != cols.end() || diags.find(diag_const) != diags.end() || antiDiags.find(antidiag_const) != antiDiags.end()) continue;
+
+            cols.insert(col);
+            diags.insert(diag_const);
+            antiDiags.insert(antidiag_const);
+            board[row][col] = 'Q';
+
+            solve(board, row+1);
+
+            cols.erase(col);
+            diags.erase(diag_const);
+            antiDiags.erase(antidiag_const);
+            board[row][col] = '.';
         }
     }
     vector<vector<string>> solveNQueens(int n) {
